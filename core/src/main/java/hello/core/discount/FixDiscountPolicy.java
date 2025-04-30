7a4b50b9c3863a7d8db7d3f54ff3391ec3b3834d
MinjiSeo16 <mjlove6630@naver.com>
2025-04-30 21:23:12 +0900

    의존관계 자동 주입

--- core/src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ core/src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -1,8 +1,15 @@
 package hello.core.discount;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
 import hello.core.member.Grade;
 import hello.core.member.Member;
 
+@Component
+@Primary
+@Qualifier("fixDiscountPolicy")
 public class FixDiscountPolicy implements DiscountPolicy{
 
     private int discountFixAmount = 1000;  //1000원 할인
--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,28 +3,48 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+// @RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     //private final MemberRepository memberRepository = new MemoryMemberRepository();
     //private final DiscountPolicy discountPolicy = new FIxDiscountPolicy();
     //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    // @Autowired 필드 주입
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+    /* Setter 주입
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy){
+    public void setMemberRepository(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    }*/
+
+    /* 일반 메서드 주입
+    @Autowired
+    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }*/
+
+    // 생성자 주입
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, /*@Qualifier("fixDiscountPolicy"*/ DiscountPolicy discountPolicy){
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
 
-    //테스트 용도
-    public MemberRepository getMemberRepository(){
-        return memberRepository;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice){
